d5852fb691ab1be5b0afc1821c5fbcc81322393d
linda <95432852+lindalacic@users.noreply.github.com>
2025-05-14 13:18:14 +0200

    Update index.js remove token

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const startScript = async () => {
 
-    const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4MzQ2Nzg5ODA5IiwibmFtZSI6IlJpY2sgUiIsImlhdCI6MTcyMzEwMjcyMn0.XRW52ihVZ7uEh_1BmZJ-jD7Cmifxbj5J74CtLp0QWp4'
-
     const res = await axios.get('https://non-existing.app/api/random', {
         headers: {
             'Authorization': AUTH_TOKEN,
